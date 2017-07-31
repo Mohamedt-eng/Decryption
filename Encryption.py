c22f7652d4febe8ddc8592f9ea8ceb1533e736f6
mohamed-BG <30345970+mohamed-BG@users.noreply.github.com>
2017-07-31 07:17:18 -0400

    Encryption-Hash

---
+++ Encryption.py
@@ -0,0 +1,21 @@
+from colorama import *
+import sys,hashlib,re
+import ctypes 
+h = hashlib.algorithms_available
+print("______MODUL HASH________")
+print("========================")
+for i in h :
+    print i
+print("======================================================")
+
+z = raw_input( Fore.CYAN +"ENTER text :>>" + "\033[0m ")
+
+ent = raw_input(Fore.CYAN + " MODULE hashes :>>" + "\033[0m ")
+
+#print(Fore.GREEN + z + "\033[0m \n"+Fore.GREEN +ent ),"\033[0m \n"
+hash_h = hashlib.new(ent)
+z = z.encode("utf-8")
+
+hash_h.update(z) 
+
+print(Fore.GREEN + z +"\033[0m " +" is hash :  " + Fore.YELLOW + hash_h.hexdigest())
