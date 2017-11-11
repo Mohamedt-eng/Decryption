55f690b43e5c7f4477c4b95a88bc2b557cf23238
mohamed-BG <30345970+mohamed-BG@users.noreply.github.com>
2017-11-11 17:20:33 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,14 +9,14 @@ mv /root/Downloads/Decryption-master/wordlist.py /usr/bin/
 
 alias Decrypt2='python3 /usr/bin/Decrypt2.py'
 alias Decrypt='python /usr/bin/Decrypt.py'
-alias Encrypt='python3 /usr/bin/Encrypt.py'
+alias Encrypt='python /usr/bin/Encrypt.py'
 alias geturl='python /usr/bin/geturl.py'
 alias wlist='python3 /usr/bin/wordlist.py'
 
 
 echo "alias Decrypt2='python3 /usr/bin/Decrypt2.py'" >> /etc/bash.bashrc
 echo "alias Decrypt='python /usr/bin/Decrypt.py'" >> /etc/bash.bashrc
-echo "alias Encrypt='python3 /usr/bin/Encrypt.py'"  >> /etc/bash.bashrc
+echo "alias Encrypt='python /usr/bin/Encrypt.py'"  >> /etc/bash.bashrc
 echo "alias geturl='python /usr/bin/geturl.py'"  >> /etc/bash.bachrc
 echo "alias wlist='python3 /usr/bin/wordlist.py'"  >> /etc/bash.bachrc
 
