991b7ee1044e6906ae1020cc310e31bd7e8c7ddc
mohamed-BG <30345970+mohamed-BG@users.noreply.github.com>
2017-11-11 15:54:25 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,14 +11,14 @@ alias Decrypt2='python3 /usr/bin/Decrypt2.py'
 alias Decrypt='python /usr/bin/Decrypt.py'
 alias Encrypt='python3 /usr/bin/Encrypt.py'
 alias geturl='python /usr/bin/geturl.py'
-alias wlist='python /usr/bin/wordlist.py'
+alias wlist='python3 /usr/bin/wordlist.py'
 
 
 echo "alias Decrypt2='python3 /usr/bin/Decrypt2.py'" >> /etc/bash.bashrc
 echo "alias Decrypt='python /usr/bin/Decrypt.py'" >> /etc/bash.bashrc
-echo "alias Encrypt='python /usr/bin/Encrypt.py'"  >> /etc/bash.bashrc
-echo " alias geturl='python /usr/bin/geturl.py'"  >> /etc/bash.bachrc
-echo " alias wlist='python /usr/bin/wordlist.py'"  >> /etc/bash.bachrc
+echo "alias Encrypt='python3 /usr/bin/Encrypt.py'"  >> /etc/bash.bashrc
+echo "alias geturl='python /usr/bin/geturl.py'"  >> /etc/bash.bachrc
+echo "alias wlist='python3 /usr/bin/wordlist.py'"  >> /etc/bash.bachrc
 
 
 
