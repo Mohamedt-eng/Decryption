0d89f20c2809e8615aeda34670558298e7c1a564
mohamed-BG <30345970+mohamed-BG@users.noreply.github.com>
2017-11-20 18:08:33 -0500

    Update install.sh
    cd /root/Downloads/Decryption-master/
    chmod +x install.sh
    ./install.sh
    ____________________
    command:
    ------------------
    Decrypt
    Decrypt2
    Encrypt
    geturl
    wordlist

--- install.sh
+++ install.sh
@@ -20,7 +20,11 @@ echo "alias Encrypt='python /usr/bin/Encrypt.py'"  >> /etc/bash.bashrc
 echo "alias geturl='python /usr/bin/geturl.py'"  >> /etc/bash.bachrc
 echo "alias wlist='python3 /usr/bin/wordlist.py'"  >> /etc/bash.bachrc
 
-
+echo "alias Decrypt2='python3 /usr/bin/Decrypt2.py'" >> /root/.bashrc
+echo "alias Decrypt='python /usr/bin/Decrypt.py'" >> /root/.bashrc
+echo "alias Encrypt='python /usr/bin/Encrypt.py'"  >> /root/.bashrc
+echo "alias geturl='python /usr/bin/geturl.py'"  >> /root/.bachrc
+echo "alias wlist='python3 /usr/bin/wordlist.py'"  >> /root/.bachrc
 
 clear
 exit
