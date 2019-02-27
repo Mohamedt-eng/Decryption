9207b742086590e1dc6a1af182f08c436e1c2909
mohamed-BG <30345970+mohamed-BG@users.noreply.github.com>
2019-02-27 08:50:33 +0100

    Update Encrypt.py

--- Encrypt.py
+++ Encrypt.py
@@ -2,15 +2,15 @@ from colorama import *
 import sys,hashlib,re
 import ctypes 
 h = hashlib.algorithms_available
-print "______MODUL HASH________"
-print "========================"
+print ("______MODUL HASH________")
+print ("========================")
 for i in h :
-    print i
-print "======================================================"
+    print (i)
+print ("==========================")
 
-z = raw_input( Fore.CYAN +"ENTER text :>>" + "\033[0m ")
+z = input( Fore.CYAN +"ENTER text :>>" + "\033[0m ")
 
-ent = raw_input(Fore.CYAN + " MODULE hashes :>>" + "\033[0m ")
+ent = input(Fore.CYAN + " MODULE hashes :>>" + "\033[0m ")
 
 #print(Fore.GREEN + z + "\033[0m \n"+Fore.GREEN +ent ),"\033[0m \n"
 hash_h = hashlib.new(ent)
@@ -18,4 +18,4 @@ z = z.encode("utf-8")
 
 hash_h.update(z) 
 
-print Fore.GREEN + z +"\033[0m " +" is hash :  " + Fore.YELLOW + hash_h.hexdigest()+"\033[0m"
+print (Fore.GREEN + str(z) +"\033[0m " +" is hash :  " + Fore.YELLOW + hash_h.hexdigest()+"\033[0m")
